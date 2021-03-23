71a69ac6f2a01162bf8f94df48aa8d189e8fad62
Anox3D <71902913+Anox3D@users.noreply.github.com>
2021-03-23 21:17:00 +0100

    Changed some things

--- js-thingies/js/ball.js
+++ js-thingies/js/ball.js
@@ -1,5 +1,5 @@
 var f = document.getElementById('ball');
 document.addEventListener('click', function(ev){
-    f.style.transform = 'translateY('+(ev.clientY-25)+'px';
-    f.style.transform += 'translateX('+(ev.clientX-25)+'px';
+    f.style.transform = 'translateY('+(ev.clientY-35)+'px';
+    f.style.transform += 'translateX('+(ev.clientX-35)+'px';
 }, false);
